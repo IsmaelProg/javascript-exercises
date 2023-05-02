dc20b60e0375074d63cc2f8ec2ed81cbaf460aef
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-02 10:13:30 +0200

    Resolver ejercicio 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    
+    for(let i=0; i < args.length; i++){
+    let indextoRemove = arr.indexOf(args[i])
+    if(indextoRemove === -1){
+        
+        } else{
+       arr.splice(indextoRemove, 1);
+             
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
