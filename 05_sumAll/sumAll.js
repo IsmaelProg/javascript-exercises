b25fb1fc0d535e06811ed1e0e4671ec024542b38
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-02 11:09:11 +0200

    resolver ejercicio 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(typeof min !== 'number' || typeof max !== 'number'){
+        return 'ERROR'
+    }else if(min >= 0 && max >= 0){
+        if(max > min){
+            for(let i = min; i <= max; i++){
+                sum += i;
+            }  
+            }else if(max < min){
+            for(let i = max; i <= min; i++){
+                sum += i;
+            } 
+            }
+    } else{
+        return 'ERROR'
+        
+    }
+    
+    
+    return sum
 };
 
 // Do not edit below this line
