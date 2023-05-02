e0ce7df36857bfa4144ab78839b05b6f55d2e83b
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-02 11:51:44 +0200

    Resolver ejercicio 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if(leapYear % 400 === 0 && leapYear % 100 === 0){
+        return true;
+    }else if(leapYear % 4 === 0 && leapYear % 100 != 0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
