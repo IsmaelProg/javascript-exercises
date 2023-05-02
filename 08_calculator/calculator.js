8f8efc232c50fe343fb03aafad95aedc31f4a004
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-02 12:54:06 +0200

    Resolver 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(...args) {
+	let result = 0;
+  for(let i = 0; i < args.length; i++){
+    result += args[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+   for(let i = 0; i < arr.length; i++){;
+   result += arr[i];
+   }
+  return result; 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr[0];
+   for(let i = 1; i < arr.length; i++){;
+   result *= arr[i];
+   }
+  return result; 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let result = num1 ** num2;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	result = 1;
+  for( let i = 1; i <= num; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
