116c9b1618633462c5788ac4ecd94be8dd52c515
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-02 02:39:28 +0200

    Primeros dos ejercicios

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    salida = ''
+    if(num < 0){
+        salida = 'ERROR';
+    }else{
+    for (i = 0; i < num; i++){
+       salida += str;
+    }
+    }
+return salida
 };
 
 // Do not edit below this line
