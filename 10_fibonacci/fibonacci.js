16bf81be357ddba9041db54795137591a69f9495
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-03 02:23:44 +0200

    Resolver ejercicio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return 'OOPS'
+    }else{
+    
+    fArr = [1, 1]; 
+    for(let i = 1; fArr.length < num; i++){
+        let newNum = fArr[i] + fArr[i - 1];
+        fArr.push(newNum)
+    }
+    return fArr[fArr.length - 1]
+}
 };
 
 // Do not edit below this line
