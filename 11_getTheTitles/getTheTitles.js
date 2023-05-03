fa57f03332bb0592bd7c87cdc563c815e3d25d81
Ismael <131832608+IsmaelProg@users.noreply.github.com>
2023-05-03 02:28:43 +0200

    Resolver ejercicio 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    newArr = [];
+    for (let i = 0; i < obj.length; i++){
+        newArr.push(obj[i].title)
+    }
+   return newArr; 
 };
 
 // Do not edit below this line
